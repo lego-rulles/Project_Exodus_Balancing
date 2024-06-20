2e75a7dde2385a0a64ae9c5da61c3982d3513816
Lego <147591747+lego-rulles@users.noreply.github.com>
2024-06-21 09:47:28 +1000

    Create ArmorTemplate.py

---
+++ Armor/ArmorTemplate.py
@@ -0,0 +1,11 @@
+# Armor Template
+ArmorName = "Armor Tempalte"
+Health = 100
+DamageResistance = 10
+HeadshotResistance = 5
+
+
+# Below if applicable
+Purchasable = true
+PurchasableLocations = "CD_Vendor"
+PurchasablePrice = 100
