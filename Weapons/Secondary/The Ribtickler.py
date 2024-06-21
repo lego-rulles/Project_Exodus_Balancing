546b4c8032a3c416a0256e36cfc6f28b75ce9afd
Lego <147591747+lego-rulles@users.noreply.github.com>
2024-06-21 18:52:59 +1000

    Create The Ribtickler.py

---
+++ Weapons/Secondary/The Ribtickler.py
@@ -0,0 +1,15 @@
+WeaponName = "The Ribtickler"
+Damage = "25"
+HeadshotDamage = "75"
+ShotsPerSecond = "1"
+Recoil = "60" # In degrees
+ReloadTime = "3" # In seconds
+AmmoAmount = "8"
+ReserveAmmo = "80"
+ShotCooldown = "2.5"
+EquipTime = "0.5"
+
+# Below if applicable
+Purchasable = false
+PurchasableLocations = ""
+PurchasablePrice = nil
