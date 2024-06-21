7d45f34d6fff000f8c00eb7b0635a24d73533ffe
Lego <147591747+lego-rulles@users.noreply.github.com>
2024-06-21 10:00:06 +1000

    Create WeaponTemplate.py

---
+++ Weapons/WeaponTemplate.py
@@ -0,0 +1,14 @@
+WeaponName = "WeaponTemplate"
+Damage = "5"
+HeadshotDamage = "10"
+ShotsPerSecond = "5"
+Recoil = "10" # In degrees
+ReloadTime = "1" # In seconds
+AmmoAmount = "50"
+ShotCooldown = "0.1"
+EquipTime = "0.5"
+
+# Below if applicable
+Purchasable = true
+PurchasableLocations = "CD_Vendor"
+PurchasablePrice = 100
