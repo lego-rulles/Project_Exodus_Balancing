1ee5e200b064b470db84bb0fe3a942b3ae261388
Lego <147591747+lego-rulles@users.noreply.github.com>
2024-06-21 18:57:49 +1000

    Create The Markiplier.py

---
+++ Weapons/Secondary/The Markiplier.py
@@ -0,0 +1,15 @@
+WeaponName = "The Markiplier"
+Damage = "35"
+HeadshotDamage = "85"
+ShotsPerSecond = "1"
+Recoil = "60" # In degrees
+ReloadTime = "2.5" # In seconds
+AmmoAmount = "12"
+ReserveAmmo = "120"
+ShotCooldown = "2"
+EquipTime = "0.5"
+
+# Below if applicable
+Purchasable = false
+PurchasableLocations = ""
+PurchasablePrice = nil
