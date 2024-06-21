eafbfd021cf9462ab2c70c20be0ac3ca61993de7
Lego <147591747+lego-rulles@users.noreply.github.com>
2024-06-21 19:02:02 +1000

    Create The Nulixier.py

---
+++ Weapons/Secondary/The Nulixier.py
@@ -0,0 +1,15 @@
+WeaponName = "The Nulixier"
+Damage = "1"
+HeadshotDamage = "5"
+ShotsPerSecond = "1"
+Recoil = "160" # In degrees
+ReloadTime = "10" # In seconds
+AmmoAmount = "2"
+ReserveAmmo = "10"
+ShotCooldown = "5"
+EquipTime = "1.5"
+
+# Below if applicable
+Purchasable = false
+PurchasableLocations = ""
+PurchasablePrice = nil
